f368fcb12bc0b24527af46d6a10476d72d5aa6fc
Badu <badu@dcard.cc>
2018-10-26 13:10:05 +0800

    setup dialog animation

--- app/src/main/java/com/crazyma/dialogfragmentsample/MainActivity.kt
+++ app/src/main/java/com/crazyma/dialogfragmentsample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.crazyma.dialogfragmentsample
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +12,26 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-    fun buttonClicked(v: View){
-        val dialogFragment = SampleDialogFragment()
+    fun buttonClicked(v: View) {
+        v.rootView
+        val array = IntArray(2)
+        v.getLocationOnScreen(array)
+        Log.d("badu", "x : ${array[0]} , y : ${array[1]} , height : ${v.height}")
+
+        val dialogFragment =
+                SampleDialogFragment.newInstance(getRelativeLeft(v), getRelativeTop(v) + v.height)
         dialogFragment.show(supportFragmentManager, "SampleDialogFragment")
     }
+
+    private fun getRelativeLeft(v: View): Int =
+            when (v.parent == v.rootView) {
+                true -> v.left
+                false -> v.left + getRelativeLeft((v.parent as View))
+            }
+
+    private fun getRelativeTop(v: View): Int =
+            when (v.parent == v.rootView) {
+                true -> v.top
+                false -> v.top + getRelativeTop((v.parent as View))
+            }
 }
--- app/src/main/java/com/crazyma/dialogfragmentsample/SampleDialogFragment.kt
+++ app/src/main/java/com/crazyma/dialogfragmentsample/SampleDialogFragment.kt
@@ -11,13 +11,35 @@ import android.view.ViewGroup
 
 class SampleDialogFragment: DialogFragment() {
 
+    companion object {
+
+        const val ARGS_POSITION_X = "ARGS_POSITION_X"
+        const val ARGS_POSITION_Y = "ARGS_POSITION_Y"
+
+        fun newInstance(positionX: Int, positionY: Int)  = SampleDialogFragment().apply {
+            arguments = Bundle().apply {
+                putInt(ARGS_POSITION_X, positionX)
+                putInt(ARGS_POSITION_Y, positionY)
+            }
+        }
+    }
+
+    var positionX = 0
+    var positionY = 0
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+         arguments?.run {
+             positionX = getInt(ARGS_POSITION_X)
+             positionY = getInt(ARGS_POSITION_Y)
+         }
 
         dialog.window?.apply {
             setGravity(Gravity.TOP or Gravity.START)
-            val p = attributes
-            p.y = 100 * 3
-            attributes = p
+            attributes = attributes!!.apply {
+                x = positionX
+                y = positionY
+            }
             setLayout(800,400)
         }
 
@@ -27,6 +49,11 @@ class SampleDialogFragment: DialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
+
+    override fun onStart() {
+        super.onStart()
+        dialog.window?.setWindowAnimations(R.style.dialog_slide_animation)
     }
 
 }
